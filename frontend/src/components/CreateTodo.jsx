a084c05aece22255c0cda3a5b62bd175aa25809b
pvinay <pinjala.vinay@soti.net>
2024-06-30 17:33:29 +0530

    added frontend

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,15 @@
+export function CreateTodo(){
+    return <div>
+        <input   style={{padding : 10, 
+            margin : 10
+        }} type="text" placeholder="title" /> <br></br>
+        <input    style={{padding : 10, 
+            margin : 10
+        }}type="text" placeholder="description" /> <br></br>
+
+        <button 
+           style={{padding : 10, 
+            margin : 10
+        }}>Add a todo</button>
+    </div>
+}
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,7 @@
+export function Todos() {
+    return <div>
+        <h1>GO to gym</h1>
+        <h2>You need to go to gym</h2>
+        <button>Mark as done</button>
+    </div>
+}
