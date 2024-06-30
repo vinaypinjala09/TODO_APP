16f72cb3c0da5f5767020e62fd58794f70ffe326
pvinay <pinjala.vinay@soti.net>
2024-06-30 16:15:06 +0530

    Intial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Todo APP
+
+This project contains a simple TODO application
+it has the following features
+
+- A User can sign up 
+- A user can sign in
+- A user can create a todo
+- A user can see their existing todos
+- A user can mark down a todo as done
---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+
+app.post("/todo",function(req,res){
+
+})
+
+
+app.get("/todos",function(req,res){
+
+})
+
+app.put("/completed",function(req,res){
+    
+})
